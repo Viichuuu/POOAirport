8d59ceb30c6fb298a15b30c27ad03606eb0f55b1
Vichu <javierlopezranero@gmail.com>
2025-04-24 13:20:02 +0200

    Base class of the AirCrafts

---
+++ src/AirCraft.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace PracticalWorkI
+{
+    public abstract class Aircraft
+    {
+        private AircraftStatus status;
+        private string id;
+        private int distance, speed;
+        private double fuelCapacity, fuelConsume, actualFuel;
+
+        public Aircraft(string id, AircraftStatus status, int distance, int speed, double fuelCapacity, double fuelConsume, double actualFuel)
+        {
+            this.id = id;
+            this.status = status;
+            this.distance = distance;
+            this.speed = speed;
+            this.fuelCapacity = fuelCapacity;
+            this.fuelConsume = fuelConsume;
+            this.actualFuel = actualFuel;
+        }
+    
+        public string GetID() => id;
+        public AircraftStatus GetStatus() => status;
+        public int GetDistance() => distance;
+        public int GetSpeed() => speed;
+        public double GetFuelCapacity() => FuelCapacity;
+        public double GetFuelConsume() => FuelConsume;
+        public double GetActualFuel() => ActualFuel;
+        public void SetStatus(AircraftStatus status) { this.status = status; }
+        public void SetDistance(int distance) { this.distance = distance; }
+        public void SetActualFuel(double actualFuel) { this.actualFuel = actualFuel; }
+
+        public override string ToString()
+        {
+            return $"ID: {id}, Status: {status}, Distance: {distance}km, Speed: {speed}km/h, Fuel: {actualFuel}/{fuelCapacity}L";
+        }
+    }
+}
