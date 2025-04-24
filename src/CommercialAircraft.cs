0dd04c97f2bc080447b01936775d8ca910092ed2
Vichu <javierlopezranero@gmail.com>
2025-04-24 13:34:02 +0200

    Plane with passengers

---
+++ src/CommercialAircraft.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace PracticalWorkI
+{
+    public class CommercialAircraft : Aircraft
+    {
+        private int passengers, //Esta es la variable caracteristica de este tipo de avion
+
+        public CommercialAircraft(string id, AircraftStatus status, int distance, int speed, double fuelCapacity, double fuelConsume, double actualFuel)
+            : base(id, status, distance, speed, fuelCapacity, fuelConsume, actualFuel)
+        {
+            this.passengers = passengers
+        }
+        public int GetPassengers() => passengers;
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Passengers: {passengers}";
+        }
+    }
+}
